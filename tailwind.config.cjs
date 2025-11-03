cec7b325f67661e8b11681a1242d1177f4ea6505
miguelseguro-22 <miguel.seguro@engitagus.com>
2025-11-03 16:55:08 +0000

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: { extend: {} },
-  plugins: [],
-}
+  plugins: []
+};
